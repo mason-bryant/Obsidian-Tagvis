0409c4a49fb77d2fa2cb31774a45c8f8488ee9f8
Mason Bryant <hamneggs@gmail.com>
2025-03-01 12:33:29 -0800

    tool tip is mostly working now.

--- src/components/tagQuery.ts
+++ src/components/tagQuery.ts
@@ -1,42 +1,50 @@
 
 export function getQuery(
-    requiredTags: string[], 
-    ignoreFilesWithTags: string[], 
+    requiredTags: string[],
+    ignoreFilesWithTags: string[],
     filterTags: string[],
-    limit: number
+    limit: number,
+    flattened: boolean = true
 ): string {
 
-
-
     var fromClause = "";
-    if(requiredTags.length > 0) {
+    if (requiredTags.length > 0) {
         fromClause = "FROM " + requiredTags.map(tag =>
-             `${tag}`)
-             .join(" AND ");
+            `${tag}`)
+            .join(" AND ");
     }
 
     var mainWhereClause = "";
-    if(ignoreFilesWithTags.length > 0) {
+    if (ignoreFilesWithTags.length > 0) {
         mainWhereClause = "WHERE " + ignoreFilesWithTags.map(tag =>
-             `contains(file.tags, "${tag}") = false AND contains(file.etags, "${tag}") = false`)
-             .join(" AND ");
+            `contains(file.tags, "${tag}") = false AND contains(file.etags, "${tag}") = false`)
+            .join(" AND ");
     }
 
-    var secondWhereClause = "";
-    if(filterTags.length > 0) {
-        secondWhereClause = "WHERE " + filterTags.map(tag =>
-             `Tag != "${tag}"`)
-             .join(" AND ");
+    if(flattened) {
+        var secondWhereClause = "";
+        if (filterTags.length > 0) {
+            secondWhereClause = "WHERE " + filterTags.map(tag =>
+                `Tag != "${tag}"`)
+                .join(" AND ");
+        }
+    
+        var query = `TABLE length(rows.file.link) AS "File Count"\n\
+            ${fromClause}\n\
+            ${mainWhereClause}\n\
+            FLATTEN file.tags AS Tag \n\
+            ${secondWhereClause}\n\
+            GROUP BY Tag \n\
+            Limit ${limit}`
+        return query;
+    
+    } else {    
+        var query = `TABLE file.name, length(rows.file.link) AS "File Count"\n\
+            ${fromClause}\n\
+            ${mainWhereClause}\n\
+            Limit ${limit}`
+        return query;
+    
     }
 
-    var query = `TABLE length(rows.file.link) AS "File Count"\n\
-${fromClause}\n\
-${mainWhereClause}\n\
-FLATTEN file.tags AS Tag \n\
-${secondWhereClause}\n\
-GROUP BY Tag \n\
-Limit ${limit}`
-
-    console.log("query", query);
-    return query;
-}
+ }
